c83bbdf58f2b7b2e0e826c528ed2837dc6af8516
ttreichl <ttreichl@student.42lausanne.ch>
2023-11-21 14:39:54 +0100

    ahhhhh

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -11,14 +11,38 @@
 /* ************************************************************************** */
 #include "get_next_line.h"
 
-int find_new_line(t_list list)
+char	*get_line(t_list *list)
 {
-	int i;
+	int		len;
+	char	*nextstring;
 
-
+	len = 0;
+	while (list->next != NULL)
+	{
+		len += ft_strlen(list->storage);
+		list = list->next;
+	}
+	while (list->next != NULL)
+	{
+		nextstring[i] = list->storage[j];
+	}
 }
 
-void	add_to_list(char *Buf, t_list *list)
+int	find_new_line(t_list *list)
+{
+	int	i;
+
+	i = 0;
+	while (i < BUFFER_SIZE)
+	{
+		if (list->storage[i] == '\n')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	add_to_list(char *Buf, t_list **list)
 {
 	t_list	*new_node;
 	t_list	*current_node;
@@ -27,20 +51,22 @@ void	add_to_list(char *Buf, t_list *list)
 	if (new_node == NULL)
 	{
 		free(new_node);
-		return;
+		return ;
 	}
 	new_node->storage = ft_strdup(Buf);
 	if (new_node->storage == NULL)
 	{
 		free(new_node->storage);
-		return;
+		return ;
 	}
-
-
-
+	new_node->next = 0;
+	current_node = *list;
+	while (current_node->next != NULL)
+		current_node = current_node->next;
+	current_node->next = new_node;
 }
 
-void	creatlist(t_list *list, int fd)
+void	creatlist(t_list **list, int fd)
 {
 	int		char_read;
 	char	*buf;
@@ -49,12 +75,12 @@ void	creatlist(t_list *list, int fd)
 	{
 		buf = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
 		if (buf == NULL)
-			return;
-		char_read = read(fd,buf, BUFFER_SIZE);
+			return ;
+		char_read = read(fd, buf, BUFFER_SIZE);
 		if (char_read == 0)
 		{
 			free(buf);
-			return;
+			return ;
 		}
 		buf[char_read] = '\0';
 		add_to_list(buf, list);
@@ -63,12 +89,14 @@ void	creatlist(t_list *list, int fd)
 
 char	*get_next_line(int fd)
 {
-	static t_list	list;
+	static t_list	*list;
+	char			*next_line
 
-
+	*list = NULL;
 	creatlist(&list, fd);
-
-
+	if (list == NULL)
+		return (0);
+	next_line = get_line(list);
 
 }
 
